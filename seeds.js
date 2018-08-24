aca6df6c0bc7105ffabf463bd68b13aa1aab9ad2
breadracer <breadracer@gmail.com>
2018-08-24 23:03:37 +0800

    Associated posts with sections

--- seeds.js
+++ seeds.js
@@ -16,10 +16,6 @@ var userData = [
   {
     name: "Charlie",
     password: "third"
-  },
-  {
-    name: "Delta",
-    password: "fourth"
   }
 ];
 
@@ -102,88 +98,45 @@ function seedDB(){
 		console.log("Registered a new user named", newUser.username);
 		counterUser++;
 		if(counterUser === userData.length){
-		  createSections();
+		  createSections(0);
 		}
-  });
-});
+	      });
+	    });
 	  }
 
-	  function createSections(){
-	    var counterSection = 0;
-	    sectionData.forEach(function(sectionName, i){
-	      Section.create({name: sectionData[i]}, function(err, newSection){
+	  function createSections(i){
+	    Section.create({name: sectionData[i]}, function(err, newSection){
+	      if(err){
+		console.log(err);
+	      }
+	      console.log("Created a new section named", newSection.name);
+	      createPosts(i, 0, newSection);
+	      if(++i !== sectionData.length){
+		setTimeout(createSections.bind(this, i), 10);
+	      }
+	    })
+	  }
+
+	  function createPosts(i, j, newSection){
+	    Post.create(postData[i][j], function(err, newPost){
+	      if(err){
+		console.log(err);
+	      }
+	      console.log("Created a new post named", newPost.name);
+	      newSection.posts.push(newPost);
+	      newSection.save(function(err, savedSection){
 		if(err){
 		  console.log(err);
 		}
-		console.log("Created a new section named", newSection.name);
-		counterSection++;
-		if(counterSection === sectionData.length){
-		  createPosts();
-		}
-  });
-});
-
-	    function createPosts(){
-	      var counterPost = 0;
-	      postData.forEach(function(postGroup, i){
-		postGroup.forEach(function(post, j){
-		  Post.create(post, function(err, newPost){
-		    if(err){
-		      console.log(err);
-		    }
-		    console.log("Created a new post named", newPost.name);
-		  });
-		});
-		counterPost++;
-		if(counterPost === postData.length){
-		  associate();
-		}
-  });
-	    }
-
-	    function associate(){
-
-	    }
-	    /* sectionData.forEach(function(sectionName, i){
-	       Section.create({name: sectionData[i]}, function(err, newSection){
-	       if(err){
-	       console.log(err)
-	       } else {
-	       console.log("Added a new section!")
-	       var newPosts = []
-	       var counterPost = 0
-	       postData[i].forEach(function(post){
-	       Post.create(post, function(err, newPost){
-	       if(err){
-	       console.log(err)
-	       }
-	       newPosts.push(newPost)
-	       console.log("Added a new post!")
-	       })
-	       counterPost++
-	       if(counterPost === postData[i].length - 1){
-	       console.log(newPosts)
-	       callBackPost()
-	       }
-	       })
-
-	       function callBackPost(){
-	       console.log(newPosts)
-	       newSection.posts.concat(newPosts)
-	       newSection.save(function(err, section){
-	       if(err){
-	       console.log(err)
-	       } else {
-	       //			console.log(section)
-	       }
-	       })
-
-	       }
-	       }
-	       })
-	       }) */
+		console.log("Saved the post!");
+	      })
+	      if(++j != postData[i].length){
+		setTimeout(createPosts.bind(this, i, j, newSection), 10);
+	      }
+	    })
 	  }
-
+	  
+	  
 	})
       })
     })
