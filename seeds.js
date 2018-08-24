d3bea0819e855a58c4cddaa43434cb8049c789d0
breadracer <breadracer@gmail.com>
2018-08-24 23:27:05 +0800

    Associated posts with sections

--- seeds.js
+++ seeds.js
@@ -16,13 +16,10 @@ var userData = [
   {
     name: "Charlie",
     password: "third"
-<<<<<<< HEAD
-=======
   },
   {
     name: "Delta",
     password: "fourth"
->>>>>>> cd9907e80edf549a75e4fe646cc83c097d72f231
   }
 ];
 
@@ -105,7 +102,6 @@ function seedDB(){
 		console.log("Registered a new user named", newUser.username);
 		counterUser++;
 		if(counterUser === userData.length){
-<<<<<<< HEAD
 		  createSections(0);
 		}
 	      });
@@ -122,7 +118,7 @@ function seedDB(){
 	      if(++i !== sectionData.length){
 		setTimeout(createSections.bind(this, i), 10);
 	      }
-	    })
+	    });
 	  }
 
 	  function createPosts(i, j, newSection){
@@ -141,98 +137,12 @@ function seedDB(){
 	      if(++j != postData[i].length){
 		setTimeout(createPosts.bind(this, i, j, newSection), 10);
 	      }
-	    })
+	    });
 	  }
-	  
-	  
-=======
-		  createSections();
-		}
+	});
+      });
+    });
   });
-});
-	  }
-
-	  function createSections(){
-	    var counterSection = 0;
-	    sectionData.forEach(function(sectionName, i){
-	      Section.create({name: sectionData[i]}, function(err, newSection){
-		if(err){
-		  console.log(err);
-		}
-		console.log("Created a new section named", newSection.name);
-		counterSection++;
-		if(counterSection === sectionData.length){
-		  createPosts();
-		}
-  });
-});
-
-	    function createPosts(){
-	      var counterPost = 0;
-	      postData.forEach(function(postGroup, i){
-		postGroup.forEach(function(post, j){
-		  Post.create(post, function(err, newPost){
-		    if(err){
-		      console.log(err);
-		    }
-		    console.log("Created a new post named", newPost.name);
-		  });
-		});
-		counterPost++;
-		if(counterPost === postData.length){
-		  associate();
-		}
-  });
-	    }
-
-	    function associate(){
-
-	    }
-	    /* sectionData.forEach(function(sectionName, i){
-	       Section.create({name: sectionData[i]}, function(err, newSection){
-	       if(err){
-	       console.log(err)
-	       } else {
-	       console.log("Added a new section!")
-	       var newPosts = []
-	       var counterPost = 0
-	       postData[i].forEach(function(post){
-	       Post.create(post, function(err, newPost){
-	       if(err){
-	       console.log(err)
-	       }
-	       newPosts.push(newPost)
-	       console.log("Added a new post!")
-	       })
-	       counterPost++
-	       if(counterPost === postData[i].length - 1){
-	       console.log(newPosts)
-	       callBackPost()
-	       }
-	       })
-
-	       function callBackPost(){
-	       console.log(newPosts)
-	       newSection.posts.concat(newPosts)
-	       newSection.save(function(err, section){
-	       if(err){
-	       console.log(err)
-	       } else {
-	       //			console.log(section)
-	       }
-	       })
-
-	       }
-	       }
-	       })
-	       }) */
-	  }
-
->>>>>>> cd9907e80edf549a75e4fe646cc83c097d72f231
-	})
-      })
-    })
-  })
 }
 
 module.exports = seedDB;
