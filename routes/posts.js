6624803768dea92cae548de6567e21f71a48abd4
breadracer <breadracer@gmail.com>
2018-08-29 23:58:47 +0800

    Added post create route

--- routes/posts.js
+++ routes/posts.js
@@ -4,10 +4,12 @@ var User = require("../models/user");
 var router = express.Router();
 var Post = require("../models/post")
 
+// SHOW
 router.get("/:postId", function(req, res){
   Post.findById(req.params.postId).populate("comment").exec(function(err, post){
     res.render("posts/show", {post: post});
   });
 });
+
 
 module.exports = router;
--- routes/sections.js
+++ routes/sections.js
@@ -1,18 +1,37 @@
 var express = require("express");
 var passport = require("passport");
 var User = require("../models/user");
-var Section = require("../models/section")
+var Section = require("../models/section");
+var bodyParser = require("body-parser");
+var Post = require("../models/post");
 var router = express.Router();
 
+
+// INDEX
 router.get("/", function(req, res){
   Section.find({}, function(err, sections){
     res.render("sections/index", {sections: sections});
   });
 });
 
+// SHOW
 router.get("/:sectionId", function(req, res){
   Section.findById(req.params.sectionId).populate("posts").exec(function(err, section){
     res.render("sections/show", {section: section});
+  });
+});
+
+// CREATE
+router.post("/:sectionId", function(req, res){
+  Post.create(req.post, function(err, post){
+    if(err){
+      console.log(err);
+    }
+    Section.findById(req.params.sectionId).populate("posts").exec(function(err, section){
+      section.posts.push(post);
+      section.save();
+      res.redirect("/sections/" + req.params.sectionId);      
+    });
   });
 });
 
